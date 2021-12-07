db3c8b86abf7224226920621d9f9b2d321c18f7d
SubhahsreeSatapathy <satapathysubhashree2000@gmail.com>
2021-12-07 19:03:23 +0530

    Add table

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,54 @@
-import React from "react";
+import * as React from "react";
 import "./App.css";
-import { Button, Table, InputGroup } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { DataGrid } from "@mui/x-data-grid";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+const columns = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 110,
+    editable: true,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.getValue(params.id, "firstName") || ""} ${
+        params.getValue(params.id, "lastName") || ""
+      }`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
 
 function Main() {
   return (
@@ -26,58 +74,15 @@ function Main() {
         </div>
 
         <div className="pt-4">
-          <Table responsive>
-            <thead>
-              <tr>
-                <th>
-                  <input
-                    type="checkbox"
-                    id="hospitalname"
-                    name="hospitalname"
-                    value="hospitalname"
-                  />
-                  Hospital Name
-                </th>
-                <th>Email address</th>
-                <th>Contact</th>
-                <th>
-                  Data Modified <i class="fas fa-arrow-down"></i>
-                </th>
-                <th>
-                  Status<i class="fas fa-arrow-down"></i>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <input
-                    type="checkbox"
-                    id="hospitalname"
-                    name="hospitalname"
-                    value="hospitalname"
-                  />
-                  SUM Hospital <br></br>
-                  <span>PVT.</span>
-                </td>
-                <td>soasom@gmail.com</td>
-                <td>
-                  +916371832434<br></br>+916371832434
-                </td>
-                <td>22 January, 2021</td>
-                <td>
-                  <i class="fas fa-circle"></i>
-                  Active
-                </td>
-                <td>
-                  <i class="far fa-trash-alt"></i>
-                  {"  "}
-                  {"  "}
-                  <i class="fas fa-pencil-alt"></i>
-                </td>
-              </tr>
-            </tbody>
-          </Table>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+            disableSelectionOnClick
+          />
+        
         </div>
       </div>
     </>
